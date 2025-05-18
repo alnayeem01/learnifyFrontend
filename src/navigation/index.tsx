254f9a1f4992c8a12afb1fd29af5e9047d321dc6
Al Nayeem <nayeem2019@hotmail.com>
2025-05-18 11:30:18 +0100

    fixed asyncStorage and implemented useEffect for better session persistency

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { FC } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { FC, useEffect } from 'react'
 import AuthNavigator from './AuthNavigator';
-import { useSelector } from 'react-redux';
-import { getAuthState } from '../store/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAuthState, updateLoggedInState, updateProfile } from '../store/auth';
 import TabNavigator from './TabNavigator';
+import { getFromAsyncStorage, keys } from '../utils/asyncStorage';
+import client from '../api/client';
 
 
 interface Props {
@@ -12,6 +13,28 @@ interface Props {
 }
 const AppNavigator: FC<Props> = props => {
     const {loggedIn} = useSelector(getAuthState)
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        const fetchAuthInfo = async()=>{
+            try{
+                const token = await getFromAsyncStorage(keys.Auth_TOKEN)
+                if(!token) return;
+                const {data} = await client.get("/auth/is-auth", {
+                    headers:{
+                        Authorization: "Bearer " + token
+                    }
+                })
+                dispatch(updateProfile(data.profile));
+                dispatch(updateLoggedInState(true))
+                console.log('Data from useEffect:',data)
+            }catch(e){
+                console.log(e)
+            }
+        }
+        fetchAuthInfo()
+    }, [])
+
+
     return (
         <NavigationContainer>
             {loggedIn? <TabNavigator /> : <AuthNavigator />}   
--- src/utils/asyncStorage.ts
+++ src/utils/asyncStorage.ts
@@ -1,13 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-
-
 export const saveToAsyncStorage = async (key: string, value: string)=>{
-    await AsyncStorage.setItem("key", value)
+    await AsyncStorage.setItem(key, value)
 };
 
-export const getFromAsyncStorage = async (key: string, value: string)=>{
-    return await AsyncStorage.getItem("key")
+export const getFromAsyncStorage = async (key: string)=>{
+    return await AsyncStorage.getItem(key)
 }
 
 export const clearAsyncStorage = async (key: string, value: string)=>{
